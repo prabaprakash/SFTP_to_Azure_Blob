e502c6d4bd7d33d576be8cff198ada217d4d1906
prabaprakash <prabakaran1993@gmail.com>
2019-01-23 14:21:31 +0530

    Tech - Express Layer

--- index.js
+++ index.js
@@ -1,204 +1,49 @@
-let Client = require("ssh2-sftp-client");
-let sftp = new Client();
-let _ = require("lodash");
-let schedule = require("node-schedule");
-let azure = require("azure");
-let serviceBusService = azure.createServiceBusService();
-var azure_storage = require("azure-storage");
-let db = require("./db");
-let config = require("./config");
-let moment = require("moment");
-const fs = require("fs");
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
-
-class sftp_to_azure {
-  constructor() {
-    this.service_bus_topic_name = "sftp_topic";
-    this.service_bus_queue_name = "sftp_queue";
-    this.sftp_config = {
-      host: config.SFTP_HOST,
-      port: config.SFTP_PORT,
-      username: config.SFTP_USERNAME,
-      password: config.SFTP_PASSWORD
-    };
-    this.sftp_from_folder = "upload/from";
-    this.sftp_to_folder = "upload/to";
-    this.azure_storage_container_url =
-      "https://prabastorage.blob.core.windows.net/prabafiles/";
-    this.azure_storage_container_name = "prabafiles";
-  }
-  async start() {
-    await sftp.connect(this.sftp_config);
-    sftp.on("end", () => {
-      console.log("sftp end event");
-    });
-    sftp.on("close", () => {
-      console.log("sftp close event");
-    });
-    //this.getSFTPFilesListAndSendToServiceBus();
-    schedule.scheduleJob("*/1 * * * *", date => {
-      console.log(`${date} - The answer to life, the universe, and everything!`);
-      this.getSFTPFilesListAndSendToServiceBus();
-    });
-  }
-
-  async fetchSFTPFiletoLocalThenPushToAzureBlob(body) {
-    const fileName = body.name;
-    //console.log(body);
-    let sftp_result = await sftp.fastGet(
-      this.sftp_from_folder + "/" + fileName,
-      "./tmp/" + fileName
-    );
-    console.log(sftp_result);
-    var blobService = azure_storage.createBlobService();
-    blobService.createBlockBlobFromLocalFile(
-      this.azure_storage_container_name,
-      fileName,
-      "./tmp/" + fileName,
-      async (error, result) => {
-        if (!error) {
-          console.log(result);
-          const deleteTmpFile = path =>
-            new Promise((res, rej) => {
-              fs.unlink(path, (err, data) => {
-                if (err) rej(err);
-                else res(data);
-              });
-            });
-          await deleteTmpFile("./tmp/" + fileName);
-          await db.files.update({
-            status: "done",
-            url: this.azure_storage_container_url + fileName,
-            updated_at: moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
-          }, {
-            where: {
-              id: body.id
-            }
-          });
-          if (await sftp.exists(this.sftp_to_folder + "/" + fileName)) {
-            await sftp.delete(this.sftp_to_folder + "/" + fileName);
-          }
-          await sftp.rename(
-            this.sftp_from_folder + "/" + fileName,
-            this.sftp_to_folder + "/" + fileName
-          );
-        } else {
-          console.log("error createBlockBlobFromLocalFile", error);
-        }
-      }
-    );
-  }
-  async getFileWithDiffFromDB(datas) {
-    const sftp_files = _.map(datas, x => ({
-      name: x.name,
-      modifyTime: x.modifyTime.toString(),
-      size: x.size.toString()
-    }));
-    let files = await db.files.findAll({
-      raw: true,
-      where: {
-        [Op.or]: sftp_files
-      }
-    });
-    // console.log('getFileWithDiffFromDB - resultset', files);
-    return _.isEmpty(files);
-  }
-  getSFTPFilesListAndSendToServiceBus() {
-    sftp
-      .list(this.sftp_from_folder)
-      .then(async datas => {
-        _.forEach(datas, data => this.sendMessageToQueue(data));
-      })
-      .catch(err => {
-        console.log("catch error -", err);
-      });
-  }
-  createQueue() {
-    var queueOptions = {
-      MaxSizeInMegabytes: "5120",
-      DefaultMessageTimeToLive: "PT5M",
-      DeadLetteringOnMessageExpiration: "true",
-      MaxDeliveryCount: "10",
-      LockDuration: "PT2M",
-      RequiresDuplicateDetection: "true"
-    };
-    serviceBusService.createQueueIfNotExists(
-      this.service_bus_queue_name,
-      queueOptions,
-      error => {
-        if (!error) {
-          // Queue exists
-        }
-      }
-    );
-  }
-  async sendMessageToQueue(data) {
-    if (await this.getFileWithDiffFromDB([data])) {
-      const payload = {
-        name: data.name,
-        modifyTime: data.modifyTime,
-        size: data.size,
-        created_at: moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
-        status: "progress",
-        updated_at: moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
-        url: ""
-      };
-      console.log("the data info -", payload);
-      const result = await db.files.create(payload);
-      var message = {
-        body: JSON.stringify({
-          id: result.dataValues.id,
-          name: data.name
-        })
-      };
-      serviceBusService.sendQueueMessage(
-        this.service_bus_queue_name,
-        message,
-        error => {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log("Message Sended to Servcie Bus -", message);
-          }
-        }
-      );
-    }
-  }
-
-  recieveMessageFromQueue() {
-    serviceBusService.receiveQueueMessage(
-      this.service_bus_queue_name, {
-        isPeekLock: true
-      },
-      async (error, lockedMessage) => {
-        if (!error) {
-          // Message received and locked
-          // console.log(lockedMessage);
-          try {
-            console.log("Recieved Message from Servcie Bus - ", JSON.parse(lockedMessage.body));
-            await this.fetchSFTPFiletoLocalThenPushToAzureBlob(
-              JSON.parse(lockedMessage.body)
-            );
-            serviceBusService.deleteMessage(lockedMessage, deleteError => {
-              if (!deleteError) {
-                console.log("Deleted Message from Servcie Bus  - ", JSON.parse(lockedMessage.body));
-              }
-            });
-          } catch (e) {
-            console.log(
-              "Exception Occured in Func recieveMessageFromQueue - ", e);
-          }
-        }
-      }
-    );
-  }
-}
+'use strict';
+const express = require('express');
+const app = express();
+// const rp = require('request-promise');
+const bodyParser = require('body-parser')
+const expressWinston = require('express-winston');
+const winston = require('winston');
+const sftp_to_azure = require('./sftp_to_azure');
+const schedule = require("node-schedule");
 const sftp_to_azure_instance = new sftp_to_azure();
+sftp_to_azure_instance.connect();
 
-sftp_to_azure_instance.createQueue();
-setInterval(() => {
+schedule.scheduleJob("*/1 * * * *", date => {
+  console.log(`${date} - Scheduler - Receiver Invoked`);
   sftp_to_azure_instance.recieveMessageFromQueue();
-}, 1000);
+});
 
-sftp_to_azure_instance.start();
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        })
+    ]
+}));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(express.static('src/dist/'));
+
+// app.get('/', function (request, response) {
+//     response.redirect('index.html');
+// });
+
+app.get('/api/sftp/initiate', async (request, response) => {
+    await sftp_to_azure_instance.start();
+    response.json({ status: "initiated" });
+});
+
+app.get('*', function (req, res) {
+    res.sendStatus(404);
+});
+
+const port = process.env.PORT || 3200;
+app.listen(port, function () {
+    console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
---
+++ sftp_to_azure.js
@@ -0,0 +1,183 @@
+const Client = require("ssh2-sftp-client");
+const sftp = new Client();
+const _ = require("lodash");
+const schedule = require("node-schedule");
+const azure = require("azure");
+const azure_storage = require("azure-storage");
+const db = require("./db");
+const config = require("./config");
+const moment = require("moment");
+const fs = require("fs");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+const queueSvc = azure.createQueueService();
+
+class sftp_to_azure {
+  constructor() {
+    this.service_bus_topic_name = "sftp_topic";
+    this.service_bus_queue_name = "sftpqueue";
+    this.sftp_config = {
+      host: config.SFTP_HOST,
+      port: config.SFTP_PORT,
+      username: config.SFTP_USERNAME,
+      password: config.SFTP_PASSWORD
+    };
+    this.sftp_from_folder = "upload/from";
+    this.sftp_to_folder = "upload/to";
+    this.azure_storage_container_url =
+      "https://prabastorage.blob.core.windows.net/prabafiles/";
+    this.azure_storage_container_name = "prabafiles";
+  }
+  async connect() {
+    await sftp.connect(this.sftp_config);
+    sftp.on("end", () => {
+      console.log("sftp end event");
+    });
+    sftp.on("close", () => {
+      console.log("sftp close event");
+    });
+  }
+  async start() {
+    //this.getSFTPFilesListAndSendToServiceBus();
+    schedule.scheduleJob("*/1 * * * *", date => {
+      console.log(`${date} - Scheduler - Sender Invoked`);
+      this.getSFTPFilesListAndSendToServiceBus();
+    });
+  }
+
+  async fetchSFTPFiletoLocalThenPushToAzureBlob(body) {
+    const fileName = body.name;
+    //console.log(body);
+    let sftp_result = await sftp.fastGet(
+      this.sftp_from_folder + "/" + fileName,
+      "./tmp/" + fileName
+    );
+    console.log(sftp_result);
+    let blobService = azure_storage.createBlobService();
+    blobService.createBlockBlobFromLocalFile(
+      this.azure_storage_container_name,
+      fileName,
+      "./tmp/" + fileName,
+      async (error, result) => {
+        if (!error) {
+          console.log(result);
+          const deleteTmpFile = path =>
+            new Promise((res, rej) => {
+              fs.unlink(path, (err, data) => {
+                if (err) rej(err);
+                else res(data);
+              });
+            });
+          await deleteTmpFile("./tmp/" + fileName);
+          await db.files.update({
+            status: "done",
+            url: this.azure_storage_container_url + fileName,
+            updated_at: moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
+          }, {
+            where: {
+              id: body.id
+            }
+          });
+          if (await sftp.exists(this.sftp_to_folder + "/" + fileName)) {
+            await sftp.delete(this.sftp_to_folder + "/" + fileName);
+          }
+          await sftp.rename(
+            this.sftp_from_folder + "/" + fileName,
+            this.sftp_to_folder + "/" + fileName
+          );
+        } else {
+          console.log("error createBlockBlobFromLocalFile", error);
+        }
+      }
+    );
+  }
+  async getFileWithDiffFromDB(datas) {
+    const sftp_files = _.map(datas, x => ({
+      name: x.name,
+      modifyTime: x.modifyTime.toString(),
+      size: x.size.toString()
+    }));
+    let files = await db.files.findAll({
+      raw: true,
+      where: {
+        [Op.or]: sftp_files
+      }
+    });
+    // console.log('getFileWithDiffFromDB - resultset', files);
+    return _.isEmpty(files);
+  }
+  getSFTPFilesListAndSendToServiceBus() {
+    sftp
+      .list(this.sftp_from_folder)
+      .then(async datas => {
+        _.forEach(datas, data => this.sendMessageToQueue(data));
+      })
+      .catch(err => {
+        console.log("catch error -", err);
+      });
+  }
+  createQueue() {
+    queueSvc.createQueueIfNotExists(this.service_bus_queue_name, (error, results, response) => {
+      if (!error) {
+        // Queue created or exists
+      }
+    });
+  }
+  async sendMessageToQueue(data) {
+    if (await this.getFileWithDiffFromDB([data])) {
+      const payload = {
+        name: data.name,
+        modifyTime: data.modifyTime,
+        size: data.size,
+        created_at: moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
+        status: "progress",
+        updated_at: moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
+        url: ""
+      };
+      console.log("the data info -", payload);
+      const result = await db.files.create(payload);
+      let message = JSON.stringify({
+          id: result.dataValues.id,
+          name: data.name
+        });
+
+      queueSvc.createMessage(this.service_bus_queue_name, message, (error, results, response) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Message Sended to Servcie Bus -", message);
+          }
+      });
+
+    }
+  }
+
+  recieveMessageFromQueue() {
+    queueSvc.getMessages(this.service_bus_queue_name, {
+      numOfMessages: 10,
+      visibilityTimeout: 1 * 60
+    }, async (error, results, getResponse) => {
+      if (!error) {
+        // Messages retrieved
+        for (let index in results) {
+          // text is available in result[index].messageText
+          try {
+            let message = results[index];
+            const messageText = JSON.parse(message.messageText);
+            console.log("Recieved Message from Servcie Bus - ", messageText);
+            await this.fetchSFTPFiletoLocalThenPushToAzureBlob(messageText);
+            queueSvc.deleteMessage(this.service_bus_queue_name, message.messageId, message.popReceipt, (error, deleteResponse) => {
+              if (!error) {
+                console.log("Deleted Message from Servcie Bus  - ", messageText);
+              }
+            });
+          } catch (e) {
+            console.log(
+              "Exception Occured in Func recieveMessageFromQueue - ", e);
+          }
+        }
+      }
+    });
+  }
+}
+module.exports = sftp_to_azure;
